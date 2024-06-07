89c59ec5d5c58f558d627337864727b7d97279e4
Optsipez <115701616+Optsipez@users.noreply.github.com>
2024-06-08 00:18:28 +0400

    Update dht_from_tk.ino

--- dht_from_tk/dht_from_tk.ino
+++ dht_from_tk/dht_from_tk.ino
@@ -8,37 +8,24 @@
 #include <ThingSpeak.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <ESP32Servo.h>
-
-Servo tempKnob;
 
 // Wi-Fi credentials
-const char* ssid = "NAME";
-const char* password = "PASS";
+const char* ssid = "MDX welcomes you";
+const char* password = "MdxL0vesyou";
 
 // ThingSpeak details
-unsigned long myChannelNumber =  CHAN NUM;  // Replace with your ThingSpeak Channel Number
-const char* myReadAPIKey = "READ KEY";       // Replace with your ThingSpeak Read API Key
-
-// Servo GPIO pin
-const int servoPin = 2;
+unsigned long myChannelNumber =  2556720;  // Replace with your ThingSpeak Channel Number
+const char* myReadAPIKey = "RB2FDXS6LTL8JA4V";       // Replace with your ThingSpeak Read API Key
 
 // OLED display settings
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
-#define MAXTEMP 280
-#define MINTEMP 180
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 WiFiClient client;
 
-int angle;
-
 void setup() {
-
-  tempKnob.attach(servoPin);
-
   // Initialize serial communication
   Serial.begin(115200);
   delay(100);
@@ -69,11 +56,10 @@ void setup() {
 
 void loop() {
   // Retrieve data from ThingSpeak
-  float humidity = ThingSpeak.readFloatField(myChannelNumber, 1, myReadAPIKey);
-  float outdoorTemp = ThingSpeak.readFloatField(myChannelNumber, 2, myReadAPIKey);
-  float indoorTemp = ThingSpeak.readFloatField(myChannelNumber, 4, myReadAPIKey);
-
-  angle = map(((int)(outdoorTemp*10.0)), MAXTEMP, MINTEMP, 0, 180);
+  float officehumidity = ThingSpeak.readFloatField(myChannelNumber, 1, myReadAPIKey);
+  float officeTemp = ThingSpeak.readFloatField(myChannelNumber, 2, myReadAPIKey);
+  float labhumidity = ThingSpeak.readFloatField(myChannelNumber, 3, myReadAPIKey);
+  float labTemp = ThingSpeak.readFloatField(myChannelNumber, 4, myReadAPIKey);
 
   // Clear the display
   display.clearDisplay();
@@ -82,27 +68,28 @@ void loop() {
   display.setTextSize(1);      
   display.setTextColor(SSD1306_WHITE);  
   display.setCursor(0,0);
-  display.print("Humidity: ");
-  display.print(humidity);
+  display.print("Office HMDT: ");
+  display.print(officehumidity);
   display.println(" %");
 
   // Display Outdoor Temperature
-  display.setCursor(0, 20);
-  display.print("Outdoor Temp: ");
-  display.print(outdoorTemp);
+  display.setCursor(0, 10);
+  display.print("Office Temp: ");
+  display.print(officeTemp);
   display.println(" C");
 
+  display.setCursor(0, 20);
+  display.print("Lab HMDT: ");
+  display.print(labhumidity);
+  display.println(" C");
   // Display Indoor Temperature
-  display.setCursor(0, 40);
-  display.print("Indoor Temp: ");
-  display.print(indoorTemp);
+  display.setCursor(0, 30);
+  display.print("Lab Temp: ");
+  display.print(labTemp);
   display.println(" C");
 
   // Show the display
   display.display();
-
-  // Set servo angle
-  tempKnob.write(angle);
 
   // Wait before next update
   delay(20000); // Update every 20 seconds
